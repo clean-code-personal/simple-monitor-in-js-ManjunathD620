a30b6779b1fff9cd86e6c0dd42d77d761a3c814e
Manjunath D <82793670+ManjunathD620@users.noreply.github.com>
2023-02-10 12:33:02 +0530

    Reduced the cyclomatic complexity & Duplication

--- bms-monitor.js
+++ bms-monitor.js
@@ -1,18 +1,16 @@
-const {expect} = require('chai');
+var isInRange = require('./checkRange')
 
 function batteryIsOk(temperature, soc, charge_rate) {
-    if (temperature < 0 || temperature > 45) {
-        console.log('Temperature is out of range!');
-        return false;
-    } else if (soc < 20 || soc > 80) {
-        console.log('State of Charge is out of range!')
-        return false;
-    } else if (charge_rate > 0.8) {
-        console.log('Charge rate is out of range!');
-        return false;
-    }
-    return true;
-}
 
-expect(batteryIsOk(25, 70, 0.7)).to.be.true;
-expect(batteryIsOk(50, 85, 0)).to.be.false;
+    var checkTemperature = isInRange(temperature,0,45);
+    var checkSoc = isInRange(soc,20,80);
+    var checkChargeRate = isInRange(charge_rate,0,0.8);
+  
+
+    return {
+        temperatureBreach : !checkTemperature,
+        socBreach: !checkSoc,
+        chargeRateBreach: !checkChargeRate
+    }
+}
+module.exports = batteryIsOk;
