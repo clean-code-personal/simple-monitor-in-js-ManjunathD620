982062fafdf8c5ba71e8ae382095cbd33265807e
Manjunath D <82793670+ManjunathD620@users.noreply.github.com>
2023-02-12 16:53:00 +0530

    Added functionality for warning

--- checkRange.js
+++ checkRange.js
@@ -1,4 +1,16 @@
-module.exports = function (value,low,high){
-    return value>=low && value<=high;
+module.exports = function (value, low, high, warningTolerance, parameterType) {
+    let breach, warn;
+    let LowWarn = low + warningTolerance, HighWarn = high - warningTolerance
+
+    if (!(value >= low && value <= high))
+        breach = true, warn = undefined;
+
+    else if ((value > LowWarn && value < HighWarn))
+        breach = false, warn = "Normal"
+
+    else
+        breach = false, warn = value > low && value <= LowWarn ? "Low" : "High";
+
+    return { breach: breach, alert: warn, parameter: parameterType ,value:value }
 }
 
