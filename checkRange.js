1a32fcc9168fbf12923c21b10d84887c9a5cf6bc
Manjunath D <82793670+ManjunathD620@users.noreply.github.com>
2023-02-12 18:25:49 +0530

    Reduced complexity by using Conditional operator

--- checkRange.js
+++ checkRange.js
@@ -1,18 +1,12 @@
 module.exports = function (value, low, high, warningTolerance, parameterType) {
-    let breach, warn;
-    let LowWarn = low + warningTolerance, HighWarn = high - warningTolerance;
-    
-    if (value >= low && value <= high) {
-        breach = false;
-        if (value > LowWarn && value < HighWarn) {
-            warn = "Normal";
-        } else {
-            warn = value <= LowWarn ? "Low" : "High";
-        }
-    } else {
-        breach = true;
-        warn = undefined;
-    }
+    let breach = value < low || value > high;
+    let warn = !breach
+        ? value <= low + warningTolerance
+            ? "Low"
+            : value >= high - warningTolerance
+                ? "High"
+                : "Normal"
+        : undefined;
 
     return { breach, alert: warn, parameter: parameterType, value };
 };
