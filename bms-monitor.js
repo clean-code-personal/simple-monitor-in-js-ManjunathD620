d180e0442034ca3526529788bfcebd90969ef607
Manjunath D <82793670+ManjunathD620@users.noreply.github.com>
2023-02-12 16:55:55 +0530

    Added Extension 1 and 3

--- bms-monitor.js
+++ bms-monitor.js
@@ -1,16 +1,16 @@
-var isInRange = require('./checkRange')
+var isInRange = require('./checkRange');
+var indicator = require('./Indicator');
+var convertTemperature = require('./temperatureConverter');
 
-function batteryIsOk(temperature, soc, charge_rate) {
+function batteryIsOk(temperature, soc, charge_rate, tolerance) {
 
-    var checkTemperature = isInRange(temperature,0,45);
-    var checkSoc = isInRange(soc,20,80);
-    var checkChargeRate = isInRange(charge_rate,0,0.8);
-  
+    temperature = convertTemperature(temperature, 0, 45, "Celsius", "Celsius");
 
-    return {
-        temperatureBreach : !checkTemperature,
-        socBreach: !checkSoc,
-        chargeRateBreach: !checkChargeRate
-    }
+    var checkTemperature = isInRange(temperature.value, temperature.low, temperature.high, tolerance, "Temperature");
+    var checkSoc = isInRange(soc, 20, 80, tolerance, "Soc");
+    var checkChargeRate = isInRange(charge_rate, 0, 0.8, 0.2, "Charge Rate");
+
+    return indicator(checkTemperature, checkSoc, checkChargeRate);
 }
+
 module.exports = batteryIsOk;
