0b6092c19858101f06566d343fefa3847799b34a
Manjunath D <82793670+ManjunathD620@users.noreply.github.com>
2023-02-12 18:19:29 +0530

    Reduced Cyclomatic Complex of Indicator function

--- Indicator.js
+++ Indicator.js
@@ -1,20 +1,22 @@
-function indicator(...args){
-
+function indicator(...args) {
     var indicate = false;
-    args.forEach((Stats)=>{
-        if(Stats.breach === true) 
-            console.error(`${Stats.parameter}: \` ${Stats.value} \` is out of range!`),indicate = true;
-        else if(Stats.alert === "Normal")
-            console.log(`${Stats.parameter}: \` ${Stats.value} \` is Normal`);
-        else{
-            var message = (Stats.alert === "Low") ?`Warning: ${Stats.parameter} \` ${Stats.value} \` Approaching discharge`:`Warning: ${Stats.parameter} \` ${Stats.value} \` Approaching charge-peak`;
-            console.warn(message)
-            indicate = true;
+    args.forEach((Stats) => {
+        switch (true) {
+            case Stats.breach === true:
+                console.error(`${Stats.parameter}: \` ${Stats.value} \` is out of range!`);
+                indicate = true;
+                break;
+            case Stats.alert === "Normal":
+                console.log(`${Stats.parameter}: \` ${Stats.value} \` is Normal`);
+                break;
+            default:
+                var message = (Stats.alert === "Low") ? `Warning: ${Stats.parameter} \` ${Stats.value} \` Approaching discharge` : `Warning: ${Stats.parameter} \` ${Stats.value} \` Approaching charge-peak`;
+                console.warn(message);
+                indicate = true;
         }
     });
-
-    return (indicate === true)?"Abnormal":"Normal";
+    
+    return (indicate === true) ? "Abnormal" : "Normal";
 }
 
 module.exports = indicator;
-
