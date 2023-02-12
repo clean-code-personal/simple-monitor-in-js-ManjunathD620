9aab173faa2a898fe2a053caef89a4c194b88077
Manjunath D <82793670+ManjunathD620@users.noreply.github.com>
2023-02-12 17:03:06 +0530

    Trying to reduce cyclomati complexity

--- checkRange.js
+++ checkRange.js
@@ -1,16 +1,18 @@
 module.exports = function (value, low, high, warningTolerance, parameterType) {
     let breach, warn;
-    let LowWarn = low + warningTolerance, HighWarn = high - warningTolerance
+    let LowWarn = low + warningTolerance, HighWarn = high - warningTolerance;
+    
+    if (value >= low && value <= high) {
+        breach = false;
+        if (value > LowWarn && value < HighWarn) {
+            warn = "Normal";
+        } else {
+            warn = value <= LowWarn ? "Low" : "High";
+        }
+    } else {
+        breach = true;
+        warn = undefined;
+    }
 
-    if (!(value >= low && value <= high))
-        breach = true, warn = undefined;
-
-    else if ((value > LowWarn && value < HighWarn))
-        breach = false, warn = "Normal"
-
-    else
-        breach = false, warn = value > low && value <= LowWarn ? "Low" : "High";
-
-    return { breach: breach, alert: warn, parameter: parameterType ,value:value }
-}
-
+    return { breach, alert: warn, parameter: parameterType, value };
+};
