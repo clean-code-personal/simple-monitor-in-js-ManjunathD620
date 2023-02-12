364fbd50faf735bf494bd1e959277462066373f8
Manjunath D <82793670+ManjunathD620@users.noreply.github.com>
2023-02-12 16:46:56 +0530

    Update Indicator.js

--- Indicator.js
+++ Indicator.js
@@ -1,13 +1,20 @@
-var logMessage = require('./logMessage');
+function indicator(...args){
 
-function indicator(...args) {
-    var indicate = "Normal";
-    args.forEach((Stats) => {
-        let temp = logMessage(Stats);
-        indicate = (indicate == "Abnormal")?indicate:temp;
+    var indicate = false;
+    args.forEach((Stats)=>{
+        if(Stats.breach === true) 
+            console.error(`${Stats.parameter}: \` ${Stats.value} \` is out of range!`),indicate = true;
+        else if(Stats.alert === "Normal")
+            console.log(`${Stats.parameter}: \` ${Stats.value} \` is Normal`);
+        else{
+            var message = (Stats.alert === "Low") ?`Warning: ${Stats.parameter} \` ${Stats.value} \` Approaching discharge`:`Warning: ${Stats.parameter} \` ${Stats.value} \` Approaching charge-peak`;
+            console.warn(message)
+            indicate = true;
+        }
     });
 
-    return indicate;
+    return (indicate === true)?"Abnormal":"Normal";
 }
 
 module.exports = indicator;
+
